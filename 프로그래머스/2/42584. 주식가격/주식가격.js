33d3fd7c0c89bbf97f4ed43035ce87705aac3555
Suyeong Kim <43226931+rlatndud9090@users.noreply.github.com>
2025-01-03 18:11:18 +0900

    [level 2] Title: 주식가격, Time: 34.11 ms, Memory: 43.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,18 @@
+function solution(prices) {
+    const stack = [];
+    const result = Array(prices.length).fill(0);
+    for (let i = 0; i < prices.length; i++) {
+        while (stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+            const top = stack.pop();
+            result[top] = i - top;
+        }
+        stack.push(i);
+    }
+    
+    while(stack.length) {
+        const top = stack.pop();
+        result[top] = prices.length - 1 - top;
+    }
+    
+    return result;
+}
